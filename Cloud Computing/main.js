0c40cda39f55ee4e0e53a4e47c900552f29ab6c9
Masayaelvin <masayaelvin@gmail.com>
2023-11-14 16:57:36 +0300

    assignment

---
+++ Cloud Computing/main.js
@@ -0,0 +1,10 @@
+// Your JavaScript file (script.js)
+
+// Get the button element by its ID
+var btn = document.getElementById('btn');
+
+// Add a click event listener to the button
+btn.addEventListener('click', function() {
+    // Redirect to another page (replace 'target_page.html' with the actual page URL)
+    window.location.href = 'index.html';
+});
